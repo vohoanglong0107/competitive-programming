63042624213694d318a9bc1f0352275c2a69921f
vohoanglong0107 <vo.hoang.long@outlook.com>
2024-01-19 23:39:53 +0000

    feat: leetcode 2430

--- src/main.rs
+++ src/main.rs
@@ -5,15 +5,16 @@ use competitive_programming::*;
 fn main() {
     let mut x = String::new();
     io::stdin().read_line(&mut x).unwrap();
-    let nums = x
-        .trim()
-        .split(",")
-        .map(|i| i.parse().unwrap())
-        .collect::<Vec<i32>>();
-    x.clear();
-    io::stdin().read_line(&mut x).unwrap();
-    let k: i32 = x.trim().parse().unwrap();
-    x.clear();
+    // let nums = x
+    //     .trim()
+    //     .split(",")
+    //     .map(|i| i.parse().unwrap())
+    //     .collect::<Vec<i32>>();
+    // x.clear();
+    // io::stdin().read_line(&mut x).unwrap();
+    // let k: i32 = x.trim().parse().unwrap();
+    // x.clear();
+    let s = String::from(x.trim());
 
-    println!("{:?}", Solution::length_of_lis(nums, k))
+    println!("{:?}", Solution::delete_string(s))
 }
